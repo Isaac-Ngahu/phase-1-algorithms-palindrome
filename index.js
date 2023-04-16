6aca3761016c807c920fd18be78eb894a9e98b6e
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-04-16 13:46:59 +0300

    palindrome lab

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+  const y=Math.floor(word.length/2);
+  for(let x=0,j=(word.length-1) ;x<=y&&j>=y;x++,j--){
+   
+      if(word[x]===word[j]||x===(y+1)){
+       return word[x]===word[j] ;
+      }else{
+        return word[x]===word[j];
+      }
+  }
 }
+isPalindrome("robot")
 
 /* 
   Add your pseudocode here
+ divide word into two to provide end point
+ iterate through each letter checking the condition
+ return result of checking condition whether true or false
 */
 
 /*
   Add written explanation of your solution here
+   divide the word into two sections to give the conition an end point
+  declare a starting point for each variable and the conditions to be checked then increment
+  check to see if the conditions are met then return the result whether word is palindrom or not
 */
 
 // You can run `node index.js` to view these console logs
